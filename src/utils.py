0b63642b4c99c5bdea6c1902932642bf924f03d5
Xiaorui Su <31068215+Blair1213@users.noreply.github.com>
2025-03-04 09:49:45 -0500

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -163,3 +163,94 @@ class MedDDxLoader:
             return [self.__getitem__(i) for i in range(self.__len__())[key]]
         else:
             raise KeyError("Key type not supported.")
+            
+class AfrimedLoader:
+    def __init__(self, data='mcq_expert', dir="dataset/"):
+        print("data is {}".format(data))
+        if data == 'AfrimedQA-MCQ':
+            self.data = 'mcq_expert'
+        elif data == 'AfrimedQA-SAQ':
+            self.data = 'saq_expert' 
+        
+        benchmark = self.process_dataset(dir)
+        if self.data not in benchmark:
+            raise KeyError("{:s} not supported".format(data))
+        self.dataset = benchmark[self.data]
+        print("{} has {} queries".format(data, len(self.dataset)))
+        print(self.dataset)
+        self.index = sorted(self.dataset.keys())
+    
+    def process_dataset(self, dir):       
+        
+        dataset_name = self.data
+        datafile_name = "AfrimedQA_{}.json".format(dataset_name)
+
+        print(datafile_name)
+
+        if os.path.exists(os.path.join(dir, datafile_name)):
+            dataset = json.load(open(os.path.join(dir, datafile_name)))
+            return dataset
+        else:
+            from datasets import load_dataset
+            options = [" A: ", " B: ", " C: ", " D: ", " E: ", " F: "]
+            ds = load_dataset("intronhealth/afrimedqa_v2")['train']
+            dataset = {dataset_name: {}}
+            print("dataset is {}".format(dataset_name))
+            
+            for d in ds:
+                print(d['question_type'])
+                #if d['split'] == 'train':
+                #    continue
+                if d['tier'] != 'expert':
+                    continue
+                if d['question_type'] == 'mcq' and 'mcq' in dataset_name:
+                    choices = [v for k, v in json.loads(d["answer_options"]).items()]
+                
+                    text = d['question_clean'].strip() + "\n"
+                    for j in range(len(choices)):
+                        text += "{} {}\n".format(options[j], choices[j])
+
+                    label_index = int(d['correct_answer'][6])-1
+                    answer = chr(ord('A') + label_index)
+                    answer_content = choices[label_index]
+
+                    idx = len(dataset['mcq_expert'])
+                    dataset['mcq_expert'][idx] = {"query": text, "answer": answer, "answer_index": label_index, "answer_content": answer_content}
+                if d['question_type'] == 'saq' and 'saq' in dataset_name:
+                    
+                    text = d['question_clean'].strip() + "\n"
+                    answer = d['answer_rationale'].strip().replace('\n', ' ').replace('\r', '')
+
+                    #label_index = int(d['correct_answer'][6])-1
+                    #answer = chr(ord('A') + label_index)
+                    #answer_content = choices[label_index]
+
+                    idx = len(dataset['saq_expert'])
+                    dataset['saq_expert'][idx] = {"query": text, "answer": answer, "answer_index": None, "answer_content": None}
+
+            with open(os.path.join(dir, datafile_name), 'w') as f:
+                json.dump(dataset, f, indent=2)
+
+        return dataset
+
+    def __process_data__(self, key):
+        data = self.dataset[self.index[key]]
+
+        answer = data["answer"].strip()
+        if self.data == 'saq_expert':
+            label_index = answer
+        else:        
+            label_index = ord(answer) - ord('A')
+        
+        return {"text": data['query'], "answer": answer, "answer_index": label_index}
+
+    def __len__(self):
+        return len(self.dataset)
+    
+    def __getitem__(self, key):
+        if type(key) == int:
+            return self.__process_data__(key)
+        elif type(key) == slice:
+            return [self.__getitem__(i) for i in range(self.__len__())[key]]
+        else:
+            raise KeyError("Key type not supported.")
